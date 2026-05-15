d9a50ab099b373d6eaa30da7aa443c73f1ca7830
Janani230904 <janurgowda239@gmail.com>
2026-05-15 15:15:35 +0000

    Student Survey App Project

---
+++ src/main/java/com/college/survey/App.java
@@ -0,0 +1,25 @@
+package com.college.survey;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" STUDENT SURVEY APPLICATION ");
+        System.out.println("=================================");
+
+        String studentName = "Janani";
+        String department = "ISE";
+
+        int campusRating = 5;
+        int academicsRating = 4;
+
+        System.out.println("Student Name : " + studentName);
+        System.out.println("Department   : " + department);
+
+        System.out.println("Campus Rating     : " + campusRating);
+        System.out.println("Academics Rating  : " + academicsRating);
+
+        System.out.println("Survey Submitted Successfully!");
+    }
+}
